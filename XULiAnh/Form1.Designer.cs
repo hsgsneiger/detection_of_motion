a53a42ffbcdb4b68e5e166b42c86b319d951529f
Pham Ngoc Toan <hsgs.neiger@gmail.com>
2019-01-01 03:08:34 +0700

    first_commit

---
+++ XULiAnh/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace XULiAnh
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.videoSourcePlayer = new AForge.Controls.VideoSourcePlayer();
+            this.cbListDevices = new System.Windows.Forms.ComboBox();
+            this.startButton = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.backgroundBorder = new System.Windows.Forms.PictureBox();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.backgroundBorder)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // videoSourcePlayer
+            // 
+            this.videoSourcePlayer.Location = new System.Drawing.Point(16, 92);
+            this.videoSourcePlayer.Margin = new System.Windows.Forms.Padding(4);
+            this.videoSourcePlayer.Name = "videoSourcePlayer";
+            this.videoSourcePlayer.Size = new System.Drawing.Size(847, 438);
+            this.videoSourcePlayer.TabIndex = 0;
+            this.videoSourcePlayer.Text = "videoSourcePlayer";
+            this.videoSourcePlayer.VideoSource = null;
+            this.videoSourcePlayer.NewFrame += new AForge.Controls.VideoSourcePlayer.NewFrameHandler(this.videoSourcePlayer_NewFrame);
+            // 
+            // cbListDevices
+            // 
+            this.cbListDevices.FormattingEnabled = true;
+            this.cbListDevices.Location = new System.Drawing.Point(17, 16);
+            this.cbListDevices.Margin = new System.Windows.Forms.Padding(4);
+            this.cbListDevices.Name = "cbListDevices";
+            this.cbListDevices.Size = new System.Drawing.Size(728, 24);
+            this.cbListDevices.TabIndex = 1;
+            this.cbListDevices.SelectedIndexChanged += new System.EventHandler(this.cbListDevices_SelectedIndexChanged);
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(763, 14);
+            this.startButton.Margin = new System.Windows.Forms.Padding(4);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(100, 28);
+            this.startButton.TabIndex = 2;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(16, 92);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(847, 438);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // backgroundBorder
+            // 
+            this.backgroundBorder.Location = new System.Drawing.Point(11, 87);
+            this.backgroundBorder.Name = "backgroundBorder";
+            this.backgroundBorder.Size = new System.Drawing.Size(856, 448);
+            this.backgroundBorder.TabIndex = 7;
+            this.backgroundBorder.TabStop = false;
+            this.backgroundBorder.Visible = false;
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(616, 57);
+            this.trackBar1.Minimum = 1;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(219, 56);
+            this.trackBar1.TabIndex = 8;
+            this.trackBar1.Value = 4;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(535, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 17);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Motion Level";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(825, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 17);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "0.1%";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(879, 546);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.cbListDevices);
+            this.Controls.Add(this.videoSourcePlayer);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.backgroundBorder);
+            this.Controls.Add(this.trackBar1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Motion Detection Program";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.backgroundBorder)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private AForge.Controls.VideoSourcePlayer videoSourcePlayer;
+        private System.Windows.Forms.ComboBox cbListDevices;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox backgroundBorder;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ XULiAnh/Form1.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AForge.Controls;
+using AForge.Video;
+using AForge.Video.DirectShow;
+using AForge.Vision.Motion;
+using Accord.Video.FFMPEG;
+namespace XULiAnh
+{
+    public partial class Form1 : Form
+    {
+        private float motionLevel = (float)0.001;
+        private FilterInfoCollection videoSource;
+        private VideoFileSource videoSource2 ;
+        private VideoCaptureDevice FinalFrame;
+        private string filePath = null;
+        MotionDetector Detector1;
+        MotionDetector Detector2;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            backgroundBorder.Load("./red.jpg");
+            Detector1 = new MotionDetector
+                (
+                new TwoFramesDifferenceDetector(),
+                new MotionBorderHighlighting()
+                );
+            Detector2 = new MotionDetector
+                (
+                new TwoFramesDifferenceDetector(),
+                new MotionBorderHighlighting()
+                );
+            videoSource = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            foreach(FilterInfo source in videoSource)
+            {
+                cbListDevices.Items.Add(source.Name);
+            }
+            cbListDevices.Items.Add("Browser...");
+            cbListDevices.SelectedIndex = 0;
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            if (((Button)sender).Text == "Start")
+            {
+                if (FinalFrame != null)
+                {
+                    videoSourcePlayer.SignalToStop();
+                }
+                startButton.Text = "Stop";
+                
+                if(cbListDevices.SelectedIndex == cbListDevices.Items.Count-1) // browse
+                {
+                    pictureBox1.BringToFront();
+                    if (videoSourcePlayer.IsRunning)
+                        videoSourcePlayer.Stop();
+                    videoSource2 = new VideoFileSource(filePath);
+                    videoSource2.NewFrame += new Accord.Video.NewFrameEventHandler(video_NewFrame);
+                    // start the video source (local video)
+                    videoSource2.Start();
+                } else
+                {
+                    if (videoSource2 != null) videoSource2.SignalToStop();
+                    videoSourcePlayer.BringToFront();
+                    FinalFrame = new VideoCaptureDevice(videoSource[cbListDevices.SelectedIndex].MonikerString);
+                    //videoSourcePlayer.NewFrame += new VideoSourcePlayer.NewFrameHandler(this.videoSourcePlayer_NewFrame);
+                    videoSourcePlayer.VideoSource = FinalFrame;
+                    videoSourcePlayer.Start();
+                }
+            } else // stop
+            {
+                videoSourcePlayer.SignalToStop();
+                //videoSourcePlayer.SignalToStop();
+                if(videoSource2 != null) videoSource2.SignalToStop();
+                ((Button)sender).Text = "Start";
+            }
+
+        }
+        private void video_NewFrame(object sender, Accord.Video.NewFrameEventArgs eventArgs)
+        {
+            if (Detector1.ProcessFrame(eventArgs.Frame) > motionLevel)
+            {
+                try
+                {
+                    Invoke(new Action(() => backgroundBorder.Visible = true));
+                }
+                catch (Exception ex)
+                {
+                    System.Environment.Exit(1);
+                    //MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                try
+                {
+                    Invoke(new Action(() => backgroundBorder.Visible = false));
+                }
+                catch (Exception ex)
+                {
+                    System.Environment.Exit(1);
+                    //MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+
+            }
+            pictureBox1.Image = (Image)eventArgs.Frame.Clone();
+        }
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            //if (FinalFrame.IsRunning)
+            //{
+            //    FinalFrame.SignalToStop();
+            //}
+        }
+
+        private void videoSourcePlayer_NewFrame(object sender, ref Bitmap image)
+        {
+            if (Detector2.ProcessFrame(image) > motionLevel)
+            {
+                try
+                {
+                    Invoke(new Action(() => backgroundBorder.Visible = true));
+                }
+                catch (Exception ex)
+                {
+                    System.Environment.Exit(1);
+                    //MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                try
+                {
+                    Invoke(new Action(() => backgroundBorder.Visible = false));
+                }
+                catch (Exception ex)
+                {
+                    System.Environment.Exit(1);
+                    //MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+
+        }
+
+        private void btnBrowser_Click(object sender, EventArgs e)
+        {
+            //videoSourcePlayer.SignalToStop();
+            //if (videoSource2 != null)  videoSource2.SignalToStop();
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Filter = "All Media Files | *.wav; *.aac; *.wma; *.wmv; *.avi; *.mpg; *.mpeg; *.m1v; *.mp2; *.mp3; *.mpa; *.mpe; *.m3u; *.mp4; *.mov; *.3g2; *.3gp2; *.3gp; *.3gpp; *.m4a; *.cda; *.aif; *.aifc; *.aiff; *.mid; *.midi; *.rmi; *.mkv; *.WAV; *.AAC; *.WMA; *.WMV; *.AVI; *.MPG; *.MPEG; *.M1V; *.MP2; *.MP3; *.MPA; *.MPE; *.M3U; *.MP4; *.MOV; *.3G2; *.3GP2; *.3GP; *.3GPP; *.M4A; *.CDA; *.AIF; *.AIFC; *.AIFF; *.MID; *.MIDI; *.RMI; *.MKV";
+            dlg.ShowDialog();
+        }
+
+        private void cbListDevices_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            videoSourcePlayer.SignalToStop();
+            if (videoSource2 != null) 
+            videoSource2.SignalToStop();
+            startButton.Text = "Start";
+            //MessageBox.Show(cbListDevices.SelectedIndex.ToString());
+            if (cbListDevices.SelectedIndex == cbListDevices.Items.Count - 1) // item browse.. be selected
+            {
+                SelectVideoFromComputer();
+            }
+        }
+        private void SelectVideoFromComputer()
+        {
+            try
+            {
+                using (OpenFileDialog ofd = new OpenFileDialog() { Filter = "MP4| *.mp4", ValidateNames = true, Multiselect = false })
+                {
+                    if (ofd.ShowDialog() == DialogResult.OK) ;
+                    //cbListDevices.SelectedText = ofd.FileName.ToString();
+                    //cbListDevices.Items[cbListDevices.SelectedIndex] = ofd.FileName.ToString();
+                    if (!cbListDevices.IsHandleCreated)
+                        cbListDevices.CreateControl();
+                    Invoke(new Action(() => cbListDevices.Text = ofd.FileName.ToString()));
+                    filePath = ofd.FileName.ToString();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            motionLevel = (float)trackBar1.Value / (float)4000;
+            label2.Text =( motionLevel * 100).ToString() + "%";
+        }
+    }
+}
